f7afa19c4baf0032ce398a7b65059385c4c41edf
Alberto <albertopereagonzalez0@gmail.com>
2023-02-22 10:54:42 +0100

    correo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,22 @@
 <!-- CAROUSEL -->
 
 
-    <!-- MAIN -->
-    <div class="container-fluid">
-        
-    </div>
+    <!-- MAIN PRODUCTOS -->
+    <section class="container-xl py-5">
+        <h2 class="text-center my-5">Nuestros productos</h2>
+        <div class="row">
+            <div class="col-lg-3 col-md-4 mb-3">
+                <img src="https://picsum.photos/600/400" alt="" class="img-fluid" data-bs-toggle="modal" data-bs-target="#modal1" id="modal-imagen">
+                <h3>Titulo Producto</h3>
+                <p>400€</p>
+                <p>Valoraciones</p>
+            </div>
+            <div class="col-lg-3 col-md-4 mb-3">
+                <img src="https://picsum.photos/600/400" alt="" class="img-fluid" data-bs-toggle="modal" data-bs-target="#modal1" id="modal-imagen">
+                <h3>Titulo Producto</h3>
+                <p>400€</p>
+                <p>Valoraciones</p>
+            </div>
+        </div>
+      </section>
 @endsection
