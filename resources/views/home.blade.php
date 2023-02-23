cee007c69c5654bff67867438c3c8dbfd4052eb5
Alberto <albertopereagonzalez0@gmail.com>
2023-02-23 10:43:33 +0100

    nuevo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,8 @@
         <h2 class="text-center my-5">Nuestros productos</h2>
         <div class="row">
             @foreach ($productos as $producto)
-            <a href="{{route('product.show',$producto->id)}}" class="col-lg-3 col-md-4 mb-3" id="link">
-                <div class="fproduct">
+            <a href="{{route('product.show',$producto->id)}}" class="col-lg-3 col-md-4 mb-3 " id="link">
+                <div class="fproduct border border-secondary">
                     <img src="{{$producto->image}}" alt="" class="img-fluid" data-bs-toggle="modal" data-bs-target="#modal1" id="modal-imagen">
                     <h3>{{$producto->name}}</h3>
                     <p>{{$producto->precio}}€</p>
