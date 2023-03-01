80a90558faf2d46a38c9b172edba2576bfcff32e
Alberto <albertopereagonzalez0@gmail.com>
2023-03-01 09:29:04 +0100

    logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,9 +74,10 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        <img src="{{URL::asset('images/box-arrow-left.svg')}}">
+                                        {{ __('Cerrar Sesion') }}
                                     </a>
-
+                                    <a href="" class="dropdown-item"><img src="{{URL::asset('images/pencil.svg')}}"> Editar</a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
