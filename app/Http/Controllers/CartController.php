4fcf18554760f83dca363fa5403557c63819d6b2
Alberto <albertopereagonzalez0@gmail.com>
2023-03-01 11:57:24 +0100

    carrito

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function cart()  {
+        $cartCollection = \Cart::getContent();
+        // dd($cartCollection);
+        return view('cart')->with(['cartCollection' => $cartCollection]);;
+    }
+    public function addToCart(Request $request)
+    {
+        \Cart::add([
+            'id' => $request->id,
+            'name' => $request->name,
+            'price' => $request->precio,
+            'quantity' => $request->quantity,
+            'attributes' => array(
+                'image' => $request->image
+            )
+        ]);
+        session()->flash('success', 'Product is Added to Cart Successfully !');
+        return redirect()->route('cart.index');
+    }
+    public function updateCart(Request $request)
+    {
+        \Cart::update(
+            $request->id,
+            [
+                'quantity' => [
+                    'relative' => false,
+                    'value' => $request->quantity
+                ],
+            ]
+        );
+        session()->flash('success', 'Item Cart is Updated Successfully !');
+        return redirect()->route('cart.index');
+    }
+    public function removeCart(Request $request)
+    {
+        \Cart::remove($request->id);
+        session()->flash('success', 'Item Cart Remove Successfully !');
+        return redirect()->route('cart.index');
+    }
+    public function clearAllCart()
+    {
+        \Cart::clear();
+        session()->flash('success', 'All Item Cart Clear Successfully !');
+        return redirect()->route('inicio');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListadoProduct;
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,14 @@ Route::get('/', [ListadoProduct::class,'index'])->name('inicio');
 Route::get('/productos', [ListadoProduct::class,'index'])->name('inicio');
 
 Route::get('/productos/{id}',[ListadoProduct::class, 'show'])->name('product.show')->middleware("auth");
+
+// Rutas gestion del carrito
+Route::get('/cart', [CartController::class, 'cart'])->name('cart.index')->middleware("auth");
+Route::post('/cart', [CartController::class, 'addToCart'])->name('cart.store')->middleware("auth");
+Route::post('/update-cart', [CartController::class, 'updateCart'])->name('cart.update')->middleware("auth");
+Route::post('/remove', [CartController::class, 'removeCart'])->name('cart.remove')->middleware("auth");
+Route::post('/clear', [CartController::class, 'clearAllCart'])->name('cart.clear')->middleware("auth");
+
 
 Auth::routes();
 
